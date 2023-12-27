a31ff13b409a9a24f2323f2115f0b167c207c85c
RoyceJoseph <dvloper100@gmail.com>
2023-12-27 20:56:25 +0200

    made the footer

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+document.querySelector("h1").style.backgroundColor = "aqua";
+
+//function
+function myfunction() {
+  document.getElementById("demo").innerHTML = "Hello";
+  var name = "Royce";
+  var age = "12";
+  var num1 = 7;
+
+  console.log(name, age);
+}
+
+myfunction();
