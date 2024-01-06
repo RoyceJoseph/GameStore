ffa8fcf2dd06b2be7d5a0e4ad728a0a3cbfb13dc
RoyceJoseph <dvloper100@gmail.com>
2024-01-06 18:37:54 +0200

    i changed the layout

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,2 @@
-// document.querySelector("h1").style.backgroundColor = "aqua";
 
-// //function
-// function myfunction() {
-//   document.getElementById("demo").innerHTML = "Hello";
-//   var name = "Royce";
-//   var age = "12";
-//   var num1 = 7;
 
-//   console.log(name, age);
-// }
-
-// myfunction();
